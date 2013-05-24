f8f71f46b870acffcc5a1c59901078a4012de5f6
eng618 <eng618@fullsail.edu>
2013-05-23 21:04:12 -0400

    Refactored idnex.html. Added error page

--- Gold/js/newMain.js
+++ Gold/js/newMain.js
@@ -1,41 +1,48 @@
+// Eric Garcia
+// MIU 1305
+// Project 3
+
+
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-});	
-		
-$('#addItem').on('pageinit', function(){
+});
 
-		var myForm = $('#newMemForm');
-		    myForm.validate({
+	$('#addItem').on('pageinit', function(){
+	// $(document).ready(function(){
+		var myForm = $('#newMemForm'),
+			errorLink = $('errorLink');
+		myForm.validate({
 			invalidHandler: function(form, validator) {
+				errorLink.click();
 			},
 			submitHandler: function() {
-		var data = myForm.serializeArray();
-			storeData(data);
-		}
-	});
-	
+				var data = myForm.serializeArray();
+				storeData(data);
+			}
+		});
+
 	//any other code needed for addItem page goes here
-	
+
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
 var autofillData = function (){
-	 
+
 };
 
 var getData = function(){
-
+	console.log(data);
 };
 
 var storeData = function(data){
-	
-}; 
+
+};
 
 var	deleteItem = function (){
-			
+
 };
-					
+
 var clearLocal = function(){
 
 };
